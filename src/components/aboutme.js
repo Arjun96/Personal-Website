a7d16d726eacd284da6b570589e9ebf637e5ffb3
Arjun Luthra <luthra.arjun@gmail.com>
2020-08-06 19:09:25 -0400

    fixed the titles on each of the pages

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -5,7 +5,7 @@ class AboutMe extends Component{
     render() {
         return (
 
-            <div><h1>About Me Page</h1></div>
+            <div><h1>About Me</h1></div>
 
         )
     }
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,7 +5,7 @@ class Projects extends Component{
     render() {
         return (
 
-            <div><h1>Projects Page</h1></div>
+            <div><h1>Projects</h1></div>
 
         )
     }
