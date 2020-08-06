6c65bb50a2a5497afe95b17d7db6bd6cb49e498e
Arjun Luthra <luthra.arjun@gmail.com>
2020-08-06 19:58:41 -0400

    Updates

---
+++ precache-manifest.2998d85dc185ebb03da993e6d749fc0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e23a1878a26e2260e49c37a888dba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9787310ed9b552eafeb3",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "76cc67fbb27c5377acf3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9787310ed9b552eafeb3",
+    "url": "/static/js/2.2ca3db57.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.2ca3db57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cc67fbb27c5377acf3",
+    "url": "/static/js/main.aff76f8d.chunk.js"
+  },
+  {
+    "revision": "506adfa26ad76816c4a2",
+    "url": "/static/js/runtime-main.c34c0ec6.js"
+  }
+]);
