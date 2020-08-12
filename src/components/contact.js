86df28b6fc3284e41e6e9bed4419c977fca5abbc
Arjun Luthra <luthra.arjun@gmail.com>
2020-08-12 19:21:05 -0400

    Readded link to contact page and created a layout

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import {Grid, Cell} from 'react-mdl'
 
 class Contact extends Component{
 
     render() {
         return (
+            
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6}>Half Page</Cell>
+                    <Cell col={6}>Half Page</Cell>
+                </Grid>
 
-            <div><h1>Contact Me</h1></div>
+
+            </div>
 
         )
     }
