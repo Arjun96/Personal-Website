99357f29bf33d07c36d7d5d903c76b4f720da56b
Arjun Luthra <luthra.arjun@gmail.com>
2020-08-12 19:37:40 -0400

    First half of contact page done, description needs to be added still

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {Grid, Cell} from 'react-mdl'
+import avatar from './../documents/photo_cropped_circle.png'
 
 class Contact extends Component{
 
@@ -8,7 +9,15 @@ class Contact extends Component{
             
             <div className="contact-body">
                 <Grid className="contact-grid">
-                    <Cell col={6}>Half Page</Cell>
+                    <Cell col={6}>
+                        <h2>Arjun Luthra</h2>
+                        <img 
+                            src={avatar}
+                            alt="avatar"
+                            style={{height:'250px'}}
+                        />
+                        <p style={{width:'75%', margin:'auto', paddingTop:'1em'}}> DESCRIPTION HERE</p>
+                    </Cell>
                     <Cell col={6}>Half Page</Cell>
                 </Grid>
 
