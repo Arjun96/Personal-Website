31aa2e2b567da593502f760e4255533805db811e
Arjun Luthra <luthra.arjun@gmail.com>
2020-08-12 20:13:17 -0400

    Finished contact page, still need to add description

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Grid, Cell} from 'react-mdl'
+import {Grid, Cell, List, ListItem, ListItemContent} from 'react-mdl'
 import avatar from './../documents/photo_cropped_circle.png'
 
 class Contact extends Component{
@@ -18,7 +18,40 @@ class Contact extends Component{
                         />
                         <p style={{width:'75%', margin:'auto', paddingTop:'1em'}}> DESCRIPTION HERE</p>
                     </Cell>
-                    <Cell col={6}>Half Page</Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr></hr>
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontSize:'25px', fontFamily:'Anton'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        <a href="mailto:arjun.luthra@mail.utoronto.ca">
+                                            arjun.luthra@mail.utoronto.ca
+                                        </a>
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{fontSize:'25px', fontFamily:'Anton', color:'black'}}>
+                                        <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                        <a href="https://www.linkedin.com/in/arjun-luthra/" target="_blank" rel="noopener noreferrer" title="Linkedin">
+                                            linkedin.com/in/arjun-luthra/
+                                        </a>
+                                    </ListItemContent>
+                                </ListItem>    
+
+                                <ListItem>
+                                    <ListItemContent style={{fontSize:'25px', fontFamily:'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        <a href="tel:4162009996">
+                                            (416) 200-9996
+                                        </a>
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+                    </Cell>
                 </Grid>
 
 
