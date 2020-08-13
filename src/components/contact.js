ff0d40f678c231194822f2d11e8473ecbd83ac1d
Arjun Luthra <luthra.arjun@gmail.com>
2020-08-12 20:58:23 -0400

    Added description and changed order of contact info

--- src/components/contact.js
+++ src/components/contact.js
@@ -16,18 +16,40 @@ class Contact extends Component{
                             alt="avatar"
                             style={{height:'250px'}}
                         />
-                        <p style={{width:'75%', margin:'auto', paddingTop:'1em'}}> DESCRIPTION HERE</p>
+                        <p style={{width:'75%', margin:'auto', paddingTop:'1em'}}>
+                            
+                            Welcome to my personal website. <br/><br/>
+
+                            My name is Arjun Luthra and I have recently graduated with a degree in Computer Science at the University of Toronto as of May 2020. <br/><br/>
+                            
+                            I am currently seeking a full-time position as a Software Engineer. My skills include Java (object oriented prgramming),
+                            Python (general scripting and automation), React (web and native) as well as some computer networking experience (TCP/IP). <br/><br/>
+
+                            In my spare time, I like to help pick out parts to and help people build custom gaming computers and I also run a 100TB file server
+                            that acts as a personal backup and media server.
+                             
+                            </p>
                     </Cell>
                     <Cell col={6}>
                         <h2>Contact Me</h2>
                         <hr></hr>
                         <div className="contact-list">
                             <List>
+
                                 <ListItem>
                                     <ListItemContent style={{fontSize:'25px', fontFamily:'Anton'}}>
                                         <i className="fa fa-envelope" aria-hidden="true"/>
                                         <a href="mailto:arjun.luthra@mail.utoronto.ca">
                                             arjun.luthra@mail.utoronto.ca
+                                        </a>
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemContent style={{fontSize:'25px', fontFamily:'Anton'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        <a href="tel:4162009996">
+                                            (416) 200-9996
                                         </a>
                                     </ListItemContent>
                                 </ListItem>
@@ -41,14 +63,7 @@ class Contact extends Component{
                                     </ListItemContent>
                                 </ListItem>    
 
-                                <ListItem>
-                                    <ListItemContent style={{fontSize:'25px', fontFamily:'Anton'}}>
-                                        <i className="fa fa-phone-square" aria-hidden="true"/>
-                                        <a href="tel:4162009996">
-                                            (416) 200-9996
-                                        </a>
-                                    </ListItemContent>
-                                </ListItem>
+
                             </List>
                         </div>
                     </Cell>
