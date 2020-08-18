acb68d308aa91bcb7dbed291e26c59226b950a91
Arjun Luthra <luthra.arjun@gmail.com>
2020-08-18 00:49:36 -0400

    Added about me section to the landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-import { Grid, Cell } from 'react-mdl'
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
+import {Link} from 'react-router-dom'
 import avatar from './../documents/photo_cropped_circle.png'
+import pdf from './../documents/Arjun Luthra Resume.pdf'
+
 
 class LandingPage extends Component{
 
@@ -15,35 +18,82 @@ class LandingPage extends Component{
                             alt="avatar"
                             className="avatar-img"
                         />
-                        
-                        <div className='banner-text'>
-
-                            <h1>Software Engineer</h1>
-                            <hr/>
-                            <p>Java | Python | C/C++ | HTML/CSS | Javascript | React | NodeJS | SQL</p>
-
-                            <div className="social-links">
-
-                                {/* Linkedin */}
-                                <a href="https://www.linkedin.com/in/arjun-luthra/" target="_blank" rel="noopener noreferrer" title="Linkedin">
-                                    <i className="fa fa-linkedin-square"  aria-hidden="true"/>
-                                </a>
-
-                                {/* Github */}
-                                <a href="https://github.com/Arjun96" target="_blank" rel="noopener noreferrer" title="Github">
-                                    <i className="fa fa-github-square"  aria-hidden="true"/>
-                                </a>
-
-                                {/* Devpost */}
-                                <a href="https://devpost.com/ArjunL" target="_blank" rel="noopener noreferrer" title="Devpost">
-                                    <i class="fa fa-code"  aria-hidden="true"/>
-                                </a>
-                            </div>
-                        </div>
-                        <br/> <br/> <br/>
-                        <strong>Copyright &copy; Arjun Luthra 2020</strong>
-                        <br/> <br/> <br/>
                     </Cell>
+                    <Cell col={6}>
+
+                        <h2>About Me</h2>
+                        <hr></hr>
+                        <p style={{width:'75%', margin:'auto', paddingTop:'1em'}}>
+                            
+                            Hello, and welcome to my personal website. <br/><br/>
+
+                            My name is Arjun Luthra and I have recently graduated with a degree in Computer Science at the University of Toronto as of May 2020. <br/><br/>
+                            
+                            I'm a software engineer with full-stack experience, including React (web and native), Java (object oriented prgramming),
+                            Python (general scripting and automation), as well as some Computer Networking (TCP/IP). <br/><br/>
+
+                            Please feel free to take a look at my 
+
+                            <a href={pdf} target="_blank" rel="noopener noreferrer">resume</a>
+
+                            or past 
+
+                            <Link to="/projects">projects</Link>
+                            
+                            and if you have any further question, do not hesitate to send me an
+
+                            <a href="mailto:arjun.luthra@mail.utoronto.ca">email.</a>    
+
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+                        <h2>Contact Me</h2>
+                        <hr></hr>
+                        <div className="contact-list">
+                            <List>
+
+                                <ListItem>
+                                    <ListItemContent>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        <a href="mailto:arjun.luthra@mail.utoronto.ca">
+                                            arjun.luthra@mail.utoronto.ca
+                                        </a>
+                                    </ListItemContent>
+                                </ListItem>
+
+                                <div className="social">
+                                <ListItem>
+                                    <ListItemContent>
+                                        <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                        <a href="https://www.linkedin.com/in/arjun-luthra/" target="_blank" rel="noopener noreferrer" title="Linkedin">
+                                            linkedin.com/in/arjun-luthra/
+                                        </a>
+                                    </ListItemContent>
+                                </ListItem>   
+                                 
+                                <ListItem>
+                                    {/* Github */}
+                                    <a href="https://github.com/Arjun96" target="_blank" rel="noopener noreferrer" title="Devpost">
+                                        <i class="fa fa-github-square"  aria-hidden="true"/>
+                                        https://github.com/Arjun96
+                                    </a>
+                                </ListItem>
+                                </div>
+
+                                <ListItem>
+                                    {/* Devpost */}
+                                    <a href="https://devpost.com/ArjunL" target="_blank" rel="noopener noreferrer" title="Devpost">
+                                        <i class="fa fa-code"  aria-hidden="true"/>
+                                        https://devpost.com/ArjunL
+                                    </a>
+                                </ListItem>
+
+                            </List>
+                        </div>
+                    </Cell>
+                    <br/> <br/> <br/>
+                    <strong>Copyright &copy; Arjun Luthra 2020</strong>
+                    <br/> <br/> <br/>
                 </Grid>
 
             </div>
--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
-import Contact from './contact';
 import Projects from './projects';
 
 //Settings the routes to each of the different pages
@@ -9,7 +8,6 @@ const Main = () => (
 
     <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Projects} />
     </Switch>
 
